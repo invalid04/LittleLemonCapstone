a08e6a2023c2e245069f933bf15b9964c140d8ad
bjbeavers <5848149-bjbeavers@users.noreply.replit.com>
2023-06-19 05:25:50 +0000

    Sections

--- test/src/components/Specials.js
+++ test/src/components/Specials.js
@@ -1,18 +1,34 @@
+import { Link } from 'react-router-dom'
+import './Specials.css'
+
 function Specials() {
   return (
     <section className="specialBox">
       <div className="topRows">
-        <h1>top row</h1>
+        <h1>This Weeks Specials</h1>
+        <Link to="/" className="menuBtn">Online Menu</Link>
       </div>
       <section className="cardRow">
         <article className="specialCard">
-          <h1>This is a special</h1>
+          <img src={require("./greekSalad.jpeg")} className="cardImg"/>
+          <h1 className="cardTitle">Greek Salad</h1>
+          <h4 className="cardPrice">$11.99</h4>
+          <p className="cardDesc">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+          <Link to="/" className="orderBtn">Order for Delivery</Link>
         </article>
         <article className="specialCard">
-          <h1>This is a special</h1>
+          <img src={require("./bruschetta.jpeg")} className="cardImg"/>
+          <h1 className="cardTitle">Bruschetta</h1>
+          <h4 className="cardPrice">$11.99</h4>
+          <p className="cardDesc">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+          <Link to="/" className="orderBtn">Order for Delivery</Link>
         </article>
         <article className="specialCard">
-          <h1>This is a special</h1>
+          <img src={require("./lemonDessert.jpeg")} className="cardImg"/>
+          <h1 className="cardTitle">Lemon Dessert</h1>
+          <h4 className="cardPrice">$11.99</h4>
+          <p className="cardDesc">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+          <Link to="/" className="orderBtn">Order for Delivery</Link>
         </article>
       </section>
     </section>
